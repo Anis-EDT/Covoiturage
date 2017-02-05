220db0dc3a409963c046b3786a083eeddfc8bf5b
adel <medini.adel@gmail.com>
2017-02-05 20:18:59 +0100

    commit adel

---
+++ src/covoiturage/userBundle/Entity/temoignage.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adel
+ * Date: 04/02/2017
+ * Time: 21:05
+ */
+
+namespace covoiturage\userBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* temoignage
+*
+ * @ORM\Table(name="temoignage")
+* @ORM\Entity()
+*/
+
+class temoignage
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+    /**
+     * @ORM\ManyToOne(targetEntity="Membre")
+     */
+    private $Membre;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMembre()
+    {
+        return $this->Membre;
+    }
+
+    /**
+     * @param mixed $Membre
+     */
+    public function setMembre($Membre)
+    {
+        $this->Membre = $Membre;
+    }
+
+}
